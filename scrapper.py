f18ca09c0b1de810342872c5dbe19dbf48ed6a53
JoaoVieira97 <jonasv55@hotmail.com>
2019-10-16 00:37:05 +0100

    Changes on scrapper to get next debuts and other fixes.

--- scrapper.py
+++ scrapper.py
@@ -9,18 +9,22 @@ movies_link = nos_cinemas_url + '/pages/cartaz.aspx'
 def getMovies():
 
     r = requests.get(movies_link)
+    movies_objects = []
     if (r.status_code == 200):
         soup = BeautifulSoup(r.text, 'html5lib')
         movies_html = soup.find('ul', {'class': 'drop-list masterBorderColor'})
         movies = movies_html.find_all('a', {'class': 'list-item'})
-        #for movie in movies:
-        #    getMovie(nos_cinemas_url + movie['href'])
-        getMovie(nos_cinemas_url + movies[0]['href'])
+        for movie in movies:
+            m = getMovie(nos_cinemas_url + movie['href'], False)
+            movies_objects.append(m)
+        #getMovie(nos_cinemas_url + movies[0]['href'])
 
     else:
         print("Não foi possível obter a lista de filmes")
 
-def getMovie(movie_link):
+    return movies_objects
+
+def getMovie(movie_link, debut):
 
     r = requests.get(movie_link)
     #print(movie_link)
@@ -32,7 +36,9 @@ def getMovie(movie_link):
         
         trailer = details.find('a', {'class': 'masterTextColor trailerButton', 'target': '_blank'})
         if trailer:
-            trailer = trailer['href']
+            trailer = trailer['href'].strip()
+        else:
+            trailer = ''
         
         movie_header = details.find('div', {'class': 'info'})
         title = movie_header.find('h1').get_text().strip()
@@ -40,21 +46,27 @@ def getMovie(movie_link):
 
         general_descriptions = details.find('section', {'class': 'description'}).find_all('p')
         d = []
-        for detail in [0, 2, 3, 4, 8]:
-            text = general_descriptions[detail].get_text().strip()
-            t = re.sub(r'[^:]*:\s', '', text)
+        if len(general_descriptions) < 10:
+            array = [0, 2, 3, 4, 8]
+        else:
+            array = [0, 2, 3, 4, 8, 9] # for movies that specify technology
+        for detail in array:
+            t = general_descriptions[detail].get_text().strip()
             d.append(t)
-        genre = d[0]
-        original_title = d[1]
-        producer = d[2]
-        actors = d[3]
-        duration = d[4]
+        genre = re.sub(r'[^:]*:\s', '', d[0])
+        original_title = re.sub(r'[^:]*:\s', '', d[1])
+        producer = re.sub(r'[^:]*:\s', '', d[2])
+        actors = re.sub(r'[^:]*:\s', '', d[3])
+        duration = re.sub(r'[^:]*:\s', '', d[4]) if len(general_descriptions) < 10 or 'Duração' in d[4] else re.sub(r'[^:]*:\s', '', d[5])
 
         synopsis = details.find('section', {'class': 'sinopse'}).find('div', {'class': 'ms-rtestate-field'}).get_text()
         synopsis = re.sub(r'\s+', ' ', synopsis)
 
         schedule_html = soup.find('section', {'class': 'schedule'}).find_all('section', {'class': 'table is-hidden display-none'})
-        schedule = getSchedule(schedule_html)
+        
+        # Too heavy for testing every time
+        #if not debut: 
+        #    schedule = getSchedule(schedule_html)
 
         movie_object = {
             'title': title,
@@ -65,10 +77,11 @@ def getMovie(movie_link):
             'duration': duration,
             'synopsis': synopsis,
             'trailer_url': trailer,
-            'banner_url': banner
+            'banner_url': banner,
+            'debut': debut
         }
 
-        print(movie_object)
+        return movie_object
 
     else:
         print("Não foi possível obter as informações do filme")
@@ -113,6 +126,30 @@ def getSchedule(schedule_html):
 
     for s in sessions_objects:
         print(s)
+    return sessions_objects
+
+def getNextDebuts():
+
+    r = requests.get(nos_cinemas_url)
+    movies_objects = []
+    if (r.status_code == 200):
+        soup = BeautifulSoup(r.text, 'html5lib')
+        script = soup.find('div', {'id': 'MSOZoneCell_WebPartWPQ1'}).find('script', {'type': 'text/javascript'}).get_text()
+        moviesData = re.findall(r'moviesData = {(.*)};', script)[0]
+        movies_json = json.loads('{' + moviesData + '}')
+        for movie in movies_json['Estreias']:
+            m = getMovie(nos_cinemas_url + movie['movieLink'], True)
+            movies_objects.append(m)
+
+    else:
+        print("Não foi possível obter a lista de próximas estreias")
+
+    return movies_objects
 
 
-getMovies()
+movies = getMovies()
+next_debuts = getNextDebuts()
+for m in next_debuts:
+    movies.append(m)
+for m in movies:
+    print(m)

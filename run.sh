df500619de52d1e932372fea9e2ece6a936b87ca
MQuaresma <miguelquaresma97@gmail.com>
2019-12-03 20:27:07 +0000

    Improve setup script behaviour

--- run.sh
+++ run.sh
@@ -3,6 +3,17 @@
 ./manage.py migrate
 ./manage.py loaddata static/cinemas_fixture.json
 
+if [[ $# -eq 2 ]] && [ $1 = "-s" ] ; then
+    HOUR=$(date +"%H")
+    HOUR_P1=$(echo "($HOUR+1)%24" | bc)
+    DELTA=$2
+else
+    HOUR="05"
+    HOUR_P1="06"
+    DELTA="60"
+fi
+sed -Ee "s/time >= '[0-9]{2}:00:00' and time < '[0-9]{2}:00:00'/time >= '"$HOUR":00:00' and time < '"$HOUR_P1":00:00'/g" -i '' scrapper/scrapper_utils.py
+sed -Ee "s/minutes\=[0-9]+/minutes="$DELTA"/g" -i '' cinemas_scrapper/settings/development.py
 redis-server &
 celery -A cinemas_scrapper.celery worker -B -l info &
 ./manage.py runserver 0.0.0.0:8000

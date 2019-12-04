8856ec44ad86bae159c9a6e4ce8260f4c95c6932
MQuaresma <miguelquaresma97@gmail.com>
2019-12-04 11:54:35 +0000

    Fix run script to update weekday

--- run.sh
+++ run.sh
@@ -6,13 +6,15 @@
 if [[ $# -eq 2 ]] && [ $1 = "-s" ] ; then
     HOUR=$(date +"%H")
     HOUR_P1=$(echo "($HOUR+1)%24" | bc)
+    WEEKDAY=$(echo $(date +"%u") "-1 % 7" | bc)
     DELTA=$2
 else
     HOUR="05"
+    WEEKDAY="3"
     HOUR_P1="06"
     DELTA="60"
 fi
-sed -Ee "s/time >= '[0-9]{2}:00:00' and time < '[0-9]{2}:00:00'/time >= '"$HOUR":00:00' and time < '"$HOUR_P1":00:00'/g" -i '' scrapper/scrapper_utils.py
+sed -Ee "s/today.weekday\(\) == [0-9] and \(time >= '[0-9]{2}:00:00' and time < '[0-9]{2}:00:00'\)/today.weekday\(\) == "$WEEKDAY" and \(time >= '"$HOUR":00:00' and time < '"$HOUR_P1":00:00'\)/g" -i '' scrapper/scrapper_utils.py
 sed -Ee "s/minutes\=[0-9]+/minutes="$DELTA"/g" -i '' cinemas_scrapper/settings/development.py
 redis-server &
 celery -A cinemas_scrapper.celery worker -B -l info &

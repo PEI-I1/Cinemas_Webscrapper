65aa61742035dadd8281748c6a9c950844080764
jcm300 <zeclmartins@gmail.com>
2020-01-07 14:18:03 +0000

    Updated ALLOWED HOSTS

--- cinemas_scrapper/settings/development.py
+++ cinemas_scrapper/settings/development.py
@@ -4,7 +4,12 @@ from datetime import timedelta
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cinemaswebscrapper']
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0",
+    "cinemaswebscrapper"
+]
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases

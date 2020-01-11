fbf892ab897b6cbf7d16fa04c9680a6378f874a0
jcm300 <zeclmartins@gmail.com>
2020-01-11 18:35:26 +0000

    Now update only runs when runserver

--- scrapper/apps.py
+++ scrapper/apps.py
@@ -1,8 +1,11 @@
 from django.apps import AppConfig
+import sys
 
 class ScrapperConfig(AppConfig):
     name = 'scrapper'
 
     def ready(self):
-        from .scrapper_utils import updateDatabaseStartup
-        updateDatabaseStartup()
+        if ("manage.py" in sys.argv or "./manage.py" in sys.argv) and \
+          "runserver" in sys.argv:
+            from .scrapper_utils import updateDatabaseStartup
+            updateDatabaseStartup()
